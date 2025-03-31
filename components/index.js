c538254af990c30e8eb3508d943e58009b67e90b
rokeowmaster <rokeowmaster@gmail.com>
2025-03-31 11:04:38 +0300

    first commit

---
+++ components/index.js
@@ -0,0 +1,10 @@
+import Banner from "./Banner";
+import Footer from "./Footer";
+import Nav from "./Nav";  
+import Hero from "./Hero";
+import Sponsors from "./Sponsors";
+import Testimonials from "./Testimonials";
+import About from "./About";
+import ProductShowcase from "./ProductShowcase";
+
+export { Banner, Footer, Nav, Hero, Sponsors, Testimonials, About, ProductShowcase };
